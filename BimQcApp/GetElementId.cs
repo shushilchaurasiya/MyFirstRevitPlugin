fd75ab10ad528e61e8533557feebbdcf269dac37
Shushil Chaurasiya <heyimsp12@gmail.com>
2023-12-12 13:25:13 +0000

    Created a first command with transaction attribute.

---
+++ BimQcApp/GetElementId.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace BimQcApp
+{
+    [TransactionAttribute(TransactionMode.ReadOnly)]
+    public class GetElementId : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
