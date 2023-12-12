b6b40a336f58668a8bebea3c953c9868b5c112bb
Shushil Chaurasiya <heyimsp12@gmail.com>
2023-12-12 13:17:00 +0000

    Add project files.

---
+++ BimQcApp/GetElementId.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace BimQcApp
+{
+    public class GetElementId
+    {
+
+    }
+}
