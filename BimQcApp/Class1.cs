deb2267d9ad1224d59f511f21698e4bba6d01bd1
Shushil Chaurasiya <heyimsp12@gmail.com>
2023-12-12 13:20:00 +0000

    Reset.

--- BimQcApp/Class1.cs
+++ BimQcApp/Class1.cs
@@ -3,13 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Autodesk.Revit.DB;
-using Autodesk.Revit.UI;
-using Autodesk.Revit.Attributes;
 
 namespace BimQcApp
 {
-    public class GetElementId
+    public class Class1
     {
 
     }
